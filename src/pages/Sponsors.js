a22d5f9faf57a565d31fc306fed22d4e018df5df
Nikki Domingo <jan.domingo@obf.ateneo.edu>
2017-12-18 10:20:04 +0800

    Edit Sponsors layout

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -4,15 +4,16 @@ export default class Testimonials extends React.Component {
 	render(){
 		return(
 			<div id="sponsors-container">
-				<h1 class="pink">Partners</h1>
-				<div class="logos">
+				<h1 class="pink">Sponsors</h1>
+				<h2 class="orange">Minor Sponsors</h2>
+				<div class="logos logos-small">
+					<img alt="Smart Logo" src={require('../assets/sponsor-smart.png')} />
+				</div>
+				<h1 class="pink">In Partnership With</h1>
+				<div class="logos logos-small">
+					<img alt="Azeus Logo" src={require('../assets/sponsor-azeus.JPG')} />
 					<img alt="Rizal Library Logo" src={require('../assets/sponsor-rizal-lib.png')} />
 					<img alt="Mozilla Logo" src={require('../assets/sponsor-mozilla.png')} />
-					<img alt="Azeus Logo" src={require('../assets/sponsor-azeus.JPG')} />
-				</div>
-				<h1 class="orange">Minor Sponsors</h1>
-				<div class="logos">
-					<img alt="Rizal Library Logo" src={require('../assets/sponsor-smart.png')} />
 				</div>
 			</div>
 		);
