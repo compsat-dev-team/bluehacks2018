1d021161d95f709442007a1cfe5194ff0a20437d
Nikki Domingo <jan.domingo@obf.ateneo.edu>
2018-01-22 14:23:32 +0800

    Add sponsors and partners

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -5,6 +5,10 @@ export default class Testimonials extends React.Component {
 		return(
 			<div id="sponsors-container">
 				<h1 class="pink">Sponsors</h1>
+				<h3>Major Sponsor</h3>
+				<div class="logos logos-big">
+					<img alt="DXC Logo" src={require('../assets/sponsor-dxc.png')} />
+				</div>
 				<h3>Minor Sponsors</h3>
 				<div class="logos logos-small">
 					<img alt="Smart Logo" src={require('../assets/sponsor-smart.png')} />
@@ -23,10 +27,18 @@ export default class Testimonials extends React.Component {
 				<h1 class="pink">Partners</h1>
 				<div class="logos logos-small">
 					<img alt="Mozilla Logo" src={require('../assets/sponsor-mozilla.png')} />
-					<img alt="DevSoc Logo" src={require('../assets/sponsor-devsoc.png')} />
-					<img alt="PCOA Logo" src={require('../assets/sponsor-pcoa.png')} />
 					<img alt="Rizal Library Logo" src={require('../assets/sponsor-rizal-lib.jpg')} />
 				</div>
+				<h1 class="pink">Partner Organizations</h1>
+				<div class="logos logos-small">
+					<img alt="DevSoc Ateneo Logo" src={require('../assets/sponsor-devsocateneo.png')} />
+					<img alt="PCOA Logo" src={require('../assets/sponsor-pcoa.png')} />
+					<img alt="Developers' Society Logo" src={require('../assets/sponsor-devsoc.png')} />
+					<img alt="MISA Logo" src={require('../assets/sponsor-misa.png')} />
+					<img alt="Ciper Logo" src={require('../assets/sponsor-cipher.png')} />
+					<img alt="Lasallian Computer Society Logo" src={require('../assets/sponsor-lcs.png')} />
+				</div>
+				
 			</div>
 		);
 	}
